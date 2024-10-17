3f05c1b33e37ac3c64ed39afb8dbef1bde587aed
Stephane Lacoin (aka nxmatic) <stephane.lacoin@hyland.com>
2024-10-17 21:01:54 +0200

    make release optional in build workflows

--- .github/workflows/src/release-build.sh
+++ .github/workflows/src/release-build.sh
@@ -9,18 +9,16 @@ fi
 ulimit -n 4096
 
 # Check if xfvb is installed
-if ! command -v Xvfb &> /dev/null; then
+if ! command -v Xvfb > /dev/null; then
   if ! test "$ZEN_CROSS_COMPILING"; then
     Xvfb :2 -screen 0 1024x768x24 &
     export LLVM_PROFDATA=$HOME/.mozbuild/clang/bin/llvm-profdata
     export DISPLAY=:2
   fi
-  export ZEN_RELEASE=1
-  pnpm build 
 else
   echo "Xvfb could not be found, running without it"
   echo "ASSUMING YOU ARE RUNNING THIS ON MACOS"
   set -v
-  export ZEN_RELEASE=1
-  pnpm build
 fi
+
+env ZEN_RELEASE=${ZEN_RELEASE:-} pnpm build 
